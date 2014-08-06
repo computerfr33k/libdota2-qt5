3b1a6a8c5703fa333ab2107592a301d75369c42c
Eric Pfeiffer <eric@eric-laptop.attlocal.net>
2014-08-05 23:54:22 -0500

    little work done

--- dota2api.cpp
+++ dota2api.cpp
@@ -1,6 +1,49 @@
 #include "dota2api.h"
 
+//const variables
+const QString Dota2API::matchDetailsURL = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=<api_key>&match_id=<match_id>";
+//end const variables
 
-Dota2API::Dota2API()
+Dota2API::Dota2API(QObject *parent) : QObject(parent)
 {
+    this->lib_version = "0.1";
+    this->netManager = new QNetworkAccessManager();
+}
+
+Dota2API::~Dota2API()
+{
+    delete netManager;
+}
+
+QString Dota2API::getLibVersion()
+{
+    return this->lib_version;
+}
+
+void Dota2API::setApiKey(QString api_key)
+{
+    this->apiKey = api_key;
+}
+
+QJsonDocument Dota2API::getMatch(QString match_id)
+{
+    QString matchUrl = Dota2API::matchDetailsURL;
+    QUrl url = QUrl(matchUrl.replace("<api_key>", this->apiKey).replace("<match_id>", match_id));
+
+    this->netReply = this->netManager->get(QNetworkRequest(url));
+    QEventLoop event;
+    QObject::connect(this->netReply, SIGNAL(finished()), &event, SLOT(quit()));
+    event.exec();
+
+    QJsonDocument json = QJsonDocument::fromJson(this->netReply->readAll());
+
+    this->netReply->deleteLater();
+
+    return json;
+}
+
+QJsonDocument Dota2API::loadReplayFromFile(QString path)
+{
+
+    return QJsonDocument();
 }
--- dota2api.h
+++ dota2api.h
@@ -2,12 +2,36 @@
 #define DOTA2API_H
 
 #include "libdota2_global.h"
+#include <QObject>
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QEventLoop>
 
-class LIBDOTA2SHARED_EXPORT Dota2API
+class LIBDOTA2SHARED_EXPORT Dota2API : public QObject
 {
+    Q_OBJECT
 
 public:
-    Dota2API();
+    Dota2API(QObject *parent = 0);
+    ~Dota2API();
+
+    QString getLibVersion();
+    QJsonDocument getMatch(QString match_id);
+    void setApiKey(QString api_key);
+    QJsonDocument loadReplayFromFile(QString path /*full path including filename*/);
+
+private:
+    static QString const matchDetailsURL;
+
+    QString lib_version;
+    QString apiKey;
+    QNetworkAccessManager *netManager;
+    QNetworkRequest netRequest;
+    QNetworkReply *netReply;
 };
 
 #endif // DOTA2API_H
